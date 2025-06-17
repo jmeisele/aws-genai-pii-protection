fff43b5e0a62ab8b7dbe6e97d64bbfb1d2ab4a13
jmeisele <jmeisele@yahoo.com>
2025-06-17 18:24:57 +0000

    :construction:

--- step_function.tf
+++ step_function.tf
@@ -11,5 +11,5 @@ resource "aws_sfn_state_machine" "sfn_state_machine" {
   #   }
   # )
 
-  definition = jsonencode(file("${path.module}/state_machine.asl.json"))
+  definition = "${path.module}/state_machine.json"
 }
