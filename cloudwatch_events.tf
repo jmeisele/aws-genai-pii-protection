2c44d75fc8a83d2da3ef007896caf87bdbd9ebc9
jmeisele <jmeisele@yahoo.com>
2025-06-20 12:19:54 +0000

    :construction:

--- cloudwatch_events.tf
+++ cloudwatch_events.tf
@@ -14,9 +14,33 @@ resource "aws_cloudwatch_event_rule" "raw_bucket_objects" {
   })
 }
 
+resource "aws_cloudwatch_event_rule" "step_function_failure" {
+  name        = "capture-objects-created"
+  description = "Capture S3 Objects Created"
+  event_pattern = jsonencode({
+    source = ["aws.states"]
+    detail = {
+      stateMachineArn = ["arn:aws:states:us-east-1:288195736164:stateMachine:pii-macie-scanner-state-machine"]
+      status          = ["FAILED", "TIMED_OUT"]
+    }
+    detail-type = [
+      "Step Functions Execution Status Change"
+    ]
+  })
+}
+
 resource "aws_cloudwatch_event_target" "step_function" {
   rule      = aws_cloudwatch_event_rule.raw_bucket_objects.name
   target_id = "StepFunctionTrigger"
   arn       = aws_sfn_state_machine.sfn_state_machine.arn
   role_arn  = aws_iam_role.events.arn
 }
+
+# {
+#   "source": ["aws.states"],
+#   "detail-type": ["Step Functions Execution Status Change"],
+#   "detail": {
+#     "status": ["FAILED", "TIMED_OUT"],
+#     "stateMachineArn": ["arn:aws:states:us-east-1:288195736164:stateMachine:pii-macie-scanner-state-machine"]
+#   }
+# }
--- cloudwatch_logs.tf
+++ cloudwatch_logs.tf
@@ -28,22 +28,22 @@ resource "aws_cloudwatch_log_group" "step_function_id" {
   retention_in_days = 7
 }
 
-resource "aws_cloudwatch_metric_alarm" "macie_scan" {
-  alarm_name          = "macie_scan-lambda-error"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "Errors"
-  namespace           = "AWS/Lambda"
-  period              = "60"
-  statistic           = "Maximum"
-  threshold           = 1
-  alarm_description   = "Lambda Errored Out"
+# resource "aws_cloudwatch_metric_alarm" "macie_scan" {
+#   alarm_name          = "macie_scan-lambda-error"
+#   comparison_operator = "GreaterThanOrEqualToThreshold"
+#   evaluation_periods  = "1"
+#   metric_name         = "Errors"
+#   namespace           = "AWS/Lambda"
+#   period              = "60"
+#   statistic           = "Maximum"
+#   threshold           = 1
+#   alarm_description   = "Lambda Errored Out"
 
-  alarm_actions = [
-    "${aws_sns_topic.macie_scan.arn}",
-  ]
+#   alarm_actions = [
+#     "${aws_sns_topic.macie_scan.arn}",
+#   ]
 
-  dimensions = {
-    FunctionName = aws_lambda_function.macie_scan.function_name
-  }
-}
+#   dimensions = {
+#     FunctionName = aws_lambda_function.macie_scan.function_name
+#   }
+# }
